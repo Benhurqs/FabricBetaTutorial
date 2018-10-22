bd922800413fdcecd0946a167d1b6ff56695e3b3
Benhur Quintino de Souza <benhur@zws-benhur-souza.zoom.local>
2018-10-22 12:19:30 -0300

    Add Fabric

--- app/src/main/java/com/benhurqs/fabricbetatutorial/MainActivity.kt
+++ app/src/main/java/com/benhurqs/fabricbetatutorial/MainActivity.kt
@@ -2,11 +2,21 @@ package com.benhurqs.fabricbetatutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
+
+    fun forceCrash(view: View) {
+        throw RuntimeException("This is a crash")
+    }
+
 }
